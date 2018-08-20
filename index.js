d444e79b4ce9efa1b08f9b3c44a9b281b638f141
Jhonatan Laguna <hola@jhonatanlaguna.com>
2018-08-20 16:00:56 -0700

    Inital server

---
+++ index.js
@@ -0,0 +1,28 @@
+'use-strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost'
+});
+
+const init = async () => {
+  server.route({
+    method: 'GET',
+    path: '/fuego',
+    handler: request => {
+      return 'fuego';
+    }
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
